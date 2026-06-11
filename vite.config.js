83b6cba09768839cbfb29f9f77547fc107ead5a2
Akash Singh <akashsingh007795@gmail.com>
2026-06-11 11:18:10 +0530

    Fix GitHub Pages routing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Neuro-Match/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/Neuro-Match/'
 })
